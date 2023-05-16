a09852f11e969a6a827e22e5bd4956c28809cd6b
_Wh0ami <newEmail>
2023-05-16 16:40:51 +0800

    2023.5.16.16:40

--- SpringBoot&vue-admin-template_demo1/SpringBoot/src/main/java/com/hy/controller/FileUploadController.java
+++ SpringBoot&vue-admin-template_demo1/SpringBoot/src/main/java/com/hy/controller/FileUploadController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
-@Controller
+
 @CrossOrigin
 @RestController
 @RequestMapping("/upload")
